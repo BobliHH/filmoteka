78927abc8ad6377a89a1ce594ed1a1c8d59ba4f4
=Andrei <balasca.andrei@gmail.com>
2023-07-28 17:23:09 +0300

    added styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,48 @@
-import "./App.css";
-import {Title} from "./common/Title"
+import './App.css';
+import { Title } from './common/Title';
+import { Alert } from './common/Alert';
 export const App = () => {
   return (
     <div
-      style={
-        {
+      style={{
+        // height: '100vh',
+        // display: 'flex',
+        // justifyContent: 'center',
+        // alignItems: 'center',
+        // fontSize: 40,
+        // color: '#010101'
+      }}
+    >
+      <div
+        style={{
           // height: '100vh',
           // display: 'flex',
           // justifyContent: 'center',
           // alignItems: 'center',
           // fontSize: 40,
           // color: '#010101'
-        }
-      }
-    >
+          backgroundColor: 'black',
+          padding: 10,
+        }}
+      >
+        <Alert> ALERT!!!</Alert>
+        <Alert type="info" modifiers={[]}>
+          {' '}
+          INFO!!!
+        </Alert>
+        <Alert type="success" modifiers={['is-outlined']}>
+          {' '}
+          SUCCESS!!!
+        </Alert>
+        <Alert type="error" modifiers={['is-elevated', 'is-outlined']}>
+          {' '}
+          Error!!!
+        </Alert>
+        <Alert type="warning" modifiers={['is-elevated']}>
+          {' '}
+          Warning!!!
+        </Alert>
+      </div>
       <Title tag="h1" title="Filmoteka" />
       <Title tag="h2" title="Filmoteka" />
       <Title tag="h3" title="Filmoteka" />
@@ -21,4 +50,3 @@ export const App = () => {
     </div>
   );
 };
-
---
+++ src/components/common/Alert.js
@@ -0,0 +1,11 @@
+import './Alert.css';
+
+export const Alert = ({ children, type, modifiers = [] }) => {
+  let classes = 'alert ' + type;
+  for (let index = 0; index < modifiers.length; index++) {
+    const mod = modifiers[index];
+    classes = classes + ' ' + mod;
+  }
+
+  return <p className={classes}>{children}</p>;
+};
